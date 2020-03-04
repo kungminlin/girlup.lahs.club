b5eb929110f54ffc75716ddb81b4ffee9d5f51f6
Kyle <35588499+SDBagel@users.noreply.github.com>
2020-03-03 20:59:40 -0800

    Add ASP.NET Core Project Files
    Moved existing assets and code to /template/ folder. Added a default ASP.NET Core 3.1 MVC project with auth, Docker and VS .gitignore.

---
+++ girlup.lahs.club/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace girlup.lahs.club.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
