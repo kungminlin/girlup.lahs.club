6677e36bf347d4e57fbb3ec1926b9973c2d79327
Kyle <sdbagel@gmail.com>
2020-03-05 23:44:54 -0800

    Add Identity Scaffolding
    Added Identity Scaffolding for user registration and login. Changed login page to fit template. Removed manual registration ability.

---
+++ girlup.lahs.club/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using girlup.lahs.club.Areas.Identity.Data;
+using girlup.lahs.club.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(girlup.lahs.club.Areas.Identity.IdentityHostingStartup))]
+namespace girlup.lahs.club.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ girlup.lahs.club/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace girlup.lahs.club.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            return Page();
+        }
+    }
+}
