3ea71a69a9bee3b937327c7e8ae8cba1b790f6cf
Kyle <sdbagel@gmail.com>
2020-03-06 22:00:55 -0800

    Build bundler
    Bundle JS and CSS on build for production.

--- girlup.lahs.club/Controllers/HomeController.cs
+++ girlup.lahs.club/Controllers/HomeController.cs
@@ -11,13 +11,6 @@ namespace girlup.lahs.club.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
